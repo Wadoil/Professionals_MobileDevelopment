b250e57df4bab5ff39a7f5346b2f840e64078889
Wadoil <wadoillalichev@inbox.ru>
2025-12-03 16:59:56 +0700

    Added registration activity

--- app/src/main/java/com/example/marathon/Presentation/Registration.kt
+++ app/src/main/java/com/example/marathon/Presentation/Registration.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.marathon.R
 
-class Authorization : AppCompatActivity() {
+class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.authorisation_activity)
+        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
